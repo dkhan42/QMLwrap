df8b523b198e8b0de35f4247ea62c9c6ac170065
dkhan42 <66374468+dkhan42@users.noreply.github.com>
2023-03-20 12:17:52 -0400

    Update KRR_example.py

--- KRR_example.py
+++ KRR_example.py
@@ -14,7 +14,7 @@ param_grid={'lambda':[1e-3,1e-6,1e-9,1e-10],
 ''' 
 Global Kernel ridge regression
 '''
-from KRR import GridSearchCV, KRR_global
+from KernelRidge import GridSearchCV, KRR_global
 
 #Hyper-parameter optimization using cross-validated grid searcch
 best_params = GridSearchCV(cm[train_idx],energies[train_idx], param_grid, 
@@ -28,7 +28,7 @@ preds = KRR_global(cm[train_idx], energies[train_idx], cm[test_idx],
 ''' 
 Local Kernel ridge regression
 '''
-from KRR import GridSearchCV_local, KRR_local
+from KernelRidge import GridSearchCV_local, KRR_local
 
 #Hyper-parameter optimization using cross-validated grid searcch
 best_params = GridSearchCV_local(fchl[train_idx], charges[train_idx], energies[train_idx], 
@@ -45,5 +45,5 @@ Kernel ridge regression with a pre-computed kernel matrix
 from qml.kernels import get_local_symmetric_kernel
 K = get_local_symmetric_kernel(fchl, charges, SIGMA= 1) #calculating the kernel for the entire dataset
 
-from KRR import KRR_indexing
+from KernelRidge import KRR_indexing
 preds = KRR_indexing(K, energies[train_idx], train_idx, test_idx, lam = 1e-6)
