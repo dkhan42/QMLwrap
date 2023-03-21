31b5ddc1ff65bbd37844ab97b1961decf843b852
dkhan42 <66374468+dkhan42@users.noreply.github.com>
2023-03-21 12:41:54 -0400

    added precomputed kernel list example

--- KRR_example.py
+++ KRR_example.py
@@ -14,7 +14,7 @@ param_grid={'lambda':[1e-3,1e-6,1e-9,1e-10],
 ''' 
 Global Kernel ridge regression
 '''
-from KernelRidge import GridSearchCV, KRR_global
+from KRR import GridSearchCV, KRR_global
 
 #Hyper-parameter optimization using cross-validated grid searcch
 best_params = GridSearchCV(cm[train_idx],energies[train_idx], param_grid, 
@@ -22,13 +22,13 @@ best_params = GridSearchCV(cm[train_idx],energies[train_idx], param_grid,
 
 #performing kernel ridge regression using the best hyper-parameters found
 preds = KRR_global(cm[train_idx], energies[train_idx], cm[test_idx], 
-            best_params, best_params['lambda'], kernel='laplacian', norm=1) #use kernel = 'gaussian', norm = 2 for the gaussian kernel
+            best_params, kernel='laplacian', norm=1) #use kernel = 'gaussian', norm = 2 for the gaussian kernel
 
 
 ''' 
 Local Kernel ridge regression
 '''
-from KernelRidge import GridSearchCV_local, KRR_local
+from KRR import GridSearchCV_local, KRR_local
 
 #Hyper-parameter optimization using cross-validated grid searcch
 best_params = GridSearchCV_local(fchl[train_idx], charges[train_idx], energies[train_idx], 
@@ -36,7 +36,7 @@ best_params = GridSearchCV_local(fchl[train_idx], charges[train_idx], energies[t
 
 #performing kernel ridge regression using the best hyper-parameters found
 preds = KRR_local(fchl[train_idx], charges[train_idx], energies[train_idx], fchl[test_idx], charges[test_idx], 
-            best_params['length'], best_params['lambda'])
+            best_params)
 
 
 ''' 
@@ -45,5 +45,23 @@ Kernel ridge regression with a pre-computed kernel matrix
 from qml.kernels import get_local_symmetric_kernel
 K = get_local_symmetric_kernel(fchl, charges, SIGMA= 1) #calculating the kernel for the entire dataset
 
-from KernelRidge import KRR_indexing
+from KRR import KRR_indexing
 preds = KRR_indexing(K, energies[train_idx], train_idx, test_idx, lam = 1e-6)
+
+
+''' 
+Kernel ridge regression with a list of pre-computed kernel matrices with different sigma
+'''
+from qml.kernels import get_local_symmetric_kernels
+K_list = get_local_symmetric_kernels(fchl, charges,SIGMAS=[10**i for i in range(-4,5)]) #calculating the kernel for the entire dataset
+
+from KRR import KRR_indexing
+y, maes = [], []
+
+for K in K_list:
+    preds = KRR_indexing(K, energies[train_idx], train_idx, test_idx, lam = 1e-6)
+    error = np.mean(np.abs(preds - energies[test_idx]))
+    y.append(preds)
+    maes.append(error)
+
+best_pred = y[np.argmin(maes)] #pick the prediction with the lowest MAE, i.e. the kernel with the best sigma
